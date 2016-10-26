0e103c1c7e386f91287ec79fddc22a89a76e02fa
bealearnscode <bealearnscode@gmail.com>
2016-10-26 12:34:28 -0400

    binary digit questions

---
+++ app.js
@@ -0,0 +1,12 @@
+//What is 2^5 in binary?
+2^5 2^4 2^3 2^2 2^1
+ 32  16  8   4   2
+  1   0  0   0   0
+
+//What is 2^5 - 1 in binary?
+2^5 = 32 32-1=31
+2^5 2^4 2^3 2^2 2^1
+ 32  16  8   4   2
+  0   1  1   1   1?
+//What is the general rule for binary numbers which are or the form 2^n or 2^n - 1?
+2^n(?) start with 1? then has 0s? like in 32? the first placeholder will be 1
